c6b2a471f7dd219923cd84d67acfe9e20eb89c9a
Nyx <92603198+Nyxx6@users.noreply.github.com>
2023-01-06 11:37:33 +0100

    Update ListBorrow.java

--- Pack/ListBorrow.java
+++ Pack/ListBorrow.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+import javax.swing.ButtonGroup;
 import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -20,6 +21,7 @@ import javax.swing.border.MatteBorder;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JRadioButton;
 
 public class ListBorrow {
 
@@ -27,6 +29,7 @@ public class ListBorrow {
 	private JTable table;
 	private Db db = Db.getInstance();
 	private ArrayList<Borrowed> list = db.getAllBorrows();
+	private ButtonGroup bg;
 
 	/**
 	 * Launch the application.
@@ -44,25 +47,46 @@ public class ListBorrow {
 		});
 	}
 	
+	private DefaultTableModel add_row_to_table(DefaultTableModel model,Object rowData[],Borrowed b) {
+		rowData[0] = b.getM().getName1();
+        rowData[1] = b.getM().getName2();
+        rowData[2] = b.getB().getIsbn();
+        rowData[3] = b.getB().getTitle();
+        rowData[4] = b.getAtdate();
+        rowData[5] = b.getDuedate();
+        rowData[6] = b.getReturned();
+        rowData[7] = b.getCondition();
+        model.addRow(rowData);return model;
+	}
 	private void getBorrows() {
 		 DefaultTableModel model = (DefaultTableModel) table.getModel();
-	        Object rowData[] = new Object[10];
-	        for(int i = 0; i < list.size(); i++)
-	        {
-	            rowData[0] = list.get(i).getM().getName1();
-	            rowData[1] = list.get(i).getM().getName2();
-	            rowData[2] = list.get(i).getB().getIsbn();
-	            rowData[3] = list.get(i).getB().getTitle();
-	            rowData[4] = list.get(i).getAtdate();
-	            rowData[5] = list.get(i).getDuedate();
-	            rowData[6] = list.get(i).getReturned();
-	            rowData[7] = list.get(i).getCondition();
-	            model.addRow(rowData);
-	        }
+		 model.setRowCount(0);
+	     Object rowData[] = new Object[10];
+		 for(int i = 0; i < list.size(); i++)
+	        { model=add_row_to_table(model,rowData,list.get(i));} 
+	}
+	
+	private void getReturned() {
+		DefaultTableModel model = (DefaultTableModel) table.getModel();
+		 model.setRowCount(0);
+	     Object rowData[] = new Object[10];
+		 for(int i = 0; i < list.size(); i++)
+	        { if(list.get(i).getCondition() != null)
+			 model=add_row_to_table(model,rowData,list.get(i));} 
+	}
+	
+	private void getNotReturned() {
+		DefaultTableModel model = (DefaultTableModel) table.getModel();
+		 model.setRowCount(0);
+	     Object rowData[] = new Object[10];
+		 for(int i = 0; i < list.size(); i++)
+	        { if(list.get(i).getCondition() == null)
+			 model=add_row_to_table(model,rowData,list.get(i));} 
 	}
 	
 	private void selectRow() {                                     
         int index = table.getSelectedRow();
+        if(list.get(index).getCondition() == null) {
         frame.dispose();
 		Return rb = new Return();
         if(index>=0) {
@@ -72,7 +96,7 @@ public class ListBorrow {
 		if(list.get(index).getB().getId() < 1)
 		rb.id.setText(""); else rb.id.setText(Integer.toString(list.get(index).getB().getId()));
 		}
-        rb.frame.setVisible(true);
+        rb.frame.setVisible(true);}
     
 	}             
 
@@ -93,7 +117,7 @@ public class ListBorrow {
 		frame.getContentPane().setLayout(null);
 		
 		JButton main = new JButton("Retour");
-		main.setBounds(725, 264, 100, 25);
+		main.setBounds(20, 266, 100, 25);
 		frame.getContentPane().add(main);
 		main.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -131,10 +155,11 @@ public class ListBorrow {
 		//frame.getContentPane().add(table);
 		JScrollPane js = new JScrollPane();
 		js.setSize(750, 427);
-		js.setLocation(32, -100);
+		js.setLocation(20, 40);
 		js.setViewportView(table);
 		
 		JButton add = new JButton("Ajouter");
+		add.setLocation(780, 214);
 		add.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				frame.dispose();
@@ -146,6 +171,7 @@ public class ListBorrow {
 		add.setBorder(new MatteBorder(2, 2, 2, 2, (Color) Color.ORANGE));
 		
 		JButton ret = new JButton("Retourner");
+		ret.setLocation(167, 264);
 		ret.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				selectRow();
@@ -153,33 +179,68 @@ public class ListBorrow {
 		});
 		ret.setFont(new Font("Arial Black", Font.PLAIN, 13));
 		ret.setBorder(new MatteBorder(2, 2, 2, 2, (Color) Color.ORANGE));
+		bg = new ButtonGroup();
+		JRadioButton all = new JRadioButton("Tous");
+		all.setSelected(true);
+		all.setFont(new Font("Times New Roman", Font.PLAIN, 12));bg.add(all);
+		all.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				getBorrows();
+			}
+		});
 		
+		JRadioButton ye_ret = new JRadioButton("Empruntés seulement");
+		ye_ret.setFont(new Font("Times New Roman", Font.PLAIN, 12));bg.add(ye_ret);
+		ye_ret.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				getReturned();
+			}
+		});
+		
+		JRadioButton no_ret = new JRadioButton("Non rendus seulement");
+		no_ret.setFont(new Font("Times New Roman", Font.PLAIN, 12));bg.add(no_ret);
+		no_ret.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				getNotReturned();
+			}
+		});
 		
 		javax.swing.GroupLayout layout = new javax.swing.GroupLayout(frame.getContentPane());
 		layout.setHorizontalGroup(
 			layout.createParallelGroup(Alignment.LEADING)
 				.addGroup(layout.createSequentialGroup()
 					.addGap(20)
-					.addGroup(layout.createParallelGroup(Alignment.LEADING)
+					.addGroup(layout.createParallelGroup(Alignment.TRAILING)
 						.addGroup(layout.createSequentialGroup()
-							.addComponent(add, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE)
-							.addGap(50)
-							.addComponent(ret, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE))
-						.addComponent(js, GroupLayout.PREFERRED_SIZE, 804, GroupLayout.PREFERRED_SIZE))
+							.addComponent(all)
+							.addGap(18)
+							.addComponent(no_ret)
+							.addGap(18)
+							.addComponent(ye_ret)
+							.addGap(10))
+						.addComponent(js, GroupLayout.PREFERRED_SIZE, 804, GroupLayout.PREFERRED_SIZE)
+						.addGroup(layout.createSequentialGroup()
+							.addComponent(ret, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE)
+							.addGap(34)
+							.addComponent(add, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE)))
 					.addContainerGap(21, Short.MAX_VALUE))
 		);
 		layout.setVerticalGroup(
 			layout.createParallelGroup(Alignment.LEADING)
-				.addGroup(layout.createSequentialGroup()
-					.addGap(46)
+				.addGroup(Alignment.TRAILING, layout.createSequentialGroup()
+					.addContainerGap(34, Short.MAX_VALUE)
+					.addGroup(layout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(ye_ret)
+						.addComponent(no_ret)
+						.addComponent(all))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addComponent(js, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED, 70, Short.MAX_VALUE)
-					.addGroup(layout.createParallelGroup(Alignment.LEADING)
+					.addGap(52)
+					.addGroup(layout.createParallelGroup(Alignment.BASELINE)
 						.addComponent(add, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
 						.addComponent(ret, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE))
 					.addContainerGap())
 		);
 		frame.getContentPane().setLayout(layout);
 	}
-
 }
